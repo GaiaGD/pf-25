619433e84ddc8356a186c9a29efcce5914a5207a
GaiaGD <gaia.digregorio@gmail.com>
2026-01-03 17:57:15 -0500

    icon added for more cards

--- src/app/components/EmptyDeck/EmptyDeck.tsx
+++ src/app/components/EmptyDeck/EmptyDeck.tsx
@@ -1,9 +1,11 @@
+import Cards from "@/app/assets/cards";
 import styles from "./EmptyDeck.module.scss";
 
 export default function EmptyDeck( { emptyDeck, bringBackAllCards }: EmptyDeckProps ) {
 
 	return (
 		<div onClick={bringBackAllCards} className={`${styles.emptyDeck} ${emptyDeck ? styles.active : ''}`}>
+			<Cards className={`${styles.icon} more-cards`} />
 			<p>Can I see the cards again?</p>
 		</div>
 	);
