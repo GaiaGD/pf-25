d9644d49f45005cab2e31fbc38d43f48912cb899
GaiaGD <gaia.digregorio@gmail.com>
2026-01-03 15:21:41 -0500

    added link styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ const instrumentSerif = Instrument_Serif({
 const rubik = Rubik({
   variable: "--font-rubik",
   subsets: ["latin"],
-  weight: ["300", "400", "700"],
+  weight: ["300", "400", "500", "700"],
 });
 
 export const metadata: Metadata = {
