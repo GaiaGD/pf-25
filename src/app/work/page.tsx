4e2ec532ba4aa3c74f1f856f4f2cc741dd61cca1
GaiaGD <gaia.digregorio@gmail.com>
2026-06-29 17:20:09 -0400

    add redirect

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,13 +1,5 @@
-import Link from 'next/link';
-import { works } from '../../../works.json';
-import WorkSlider from '@/app/components/WorkSlider/WorkSlider';
-import styles from './page.module.scss';
+import { permanentRedirect } from 'next/navigation';
 
 export default function Work() {
-  return (
-    <div className={styles.page}>
-      {/* <Link href="/" className={styles.back}>← Back</Link> */}
-      <WorkSlider works={works} />
-    </div>
-  );
+  permanentRedirect('/');
 }
